6d3fc280b9fc5d0fa05595a20cc85002d84c9630
Mario Sanchez <mario.sanchez@conecta.mx>
2018-01-04 18:34:04 -0600

    changes made in package

--- src/main/java/com/myapp/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/myapp/springboot/app/models/dao/ClienteDaoImpl.java
@@ -2,10 +2,18 @@ package com.myapp.springboot.app.models.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
 import com.myapp.springboot.app.models.entity.Cliente;
 
-public class ClienteDaoImpl implements IClienteDao {
 
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+	
+	
+	private EntityManager em;
 	@Override
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
